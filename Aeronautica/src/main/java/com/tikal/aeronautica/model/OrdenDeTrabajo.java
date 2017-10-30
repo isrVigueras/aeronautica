730f9820c834f979cef083a836518a62e20424a1
gsantiago <gsantiago@xibalba>
2017-10-30 14:23:05 -0600

    First commit

---
+++ Aeronautica/src/main/java/com/tikal/aeronautica/model/OrdenDeTrabajo.java
@@ -0,0 +1,9 @@
+/**
+ * Clase pojo para representar una orden de trabajo con sus datos escenciales.
+ */
+package com.tikal.aeronautica.model;
+
+public class OrdenDeTrabajo {
+	private String empresa;
+
+}
