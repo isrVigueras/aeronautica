b4039060c235a3e043641c817f7210176ff0e64f
Tikal <Tikal@TikalEscritorio>
2017-11-30 11:44:16 -0600

    json en ordenes generadas

--- Aeronautikal/war/js/script.js
+++ Aeronautikal/war/js/script.js
@@ -1,4 +1,5 @@
 var app = angular.module('app', ['ngRoute']);
+//servicio para mandar los datos del formulario a la ruta del maping del controlador java mediante post
 app.service('OrdenesService', [ '$http', '$q', function($http, $q) {
   this.genera_orden = function(folio,fechaApertura,con_nombre,con_telefono,con_correo,empresa,a_matricula,a_modelo,n_serie,a_t_vuelo,a_t_aterrizaje) {
     var d = $q.defer();
@@ -23,53 +24,7 @@ app.service('OrdenesService', [ '$http', '$q', function($http, $q) {
     return d.promise;
   }
 } ]);
-
-app.config(['$routeProvider',function($routeProvider) {
- 
-  $routeProvider.when('/', {
-    templateUrl: "login.html",
-    controller: "MainController"
-  }); 
- 
-  $routeProvider.when('/Orden/generadas', {
-    templateUrl: "Ordenes_generadas.html",
-    controller: ""
-  });
-     
-  $routeProvider.when('/Orden/generar', {
-    templateUrl: "Orden_de_trabajo.html",
-    controller: "ordenController"
-  });
-
-  $routeProvider.when('/Orden/detalle', {
-    templateUrl: "Detalle_discrepancias.html",
-    controller: "detalledisController"
-  });
-
-    $routeProvider.when('/Orden/discrepancia', {
-    templateUrl: "Discrepancias.html",
-    controller: ""
-  });      
-        $routeProvider.when('/Inventario/alta', {
-    templateUrl: "inventario.html",
-    controller: ""
-  });     
-  $routeProvider.when('/seguro/edit/:idSeguro', {
-    templateUrl: "Orden_de_trabajo.html",
-    controller: "DetalleSeguroController",
-    resolve: {
-      seguro:['remoteResource','$route',function(remoteResource,$route) {
-        return remoteResource.get($route.current.params.idSeguro);
-      }]
-    }
-  });
-   
-  $routeProvider.otherwise({
-        redirectTo: '/'
-  });   
- 
-}]);
-
+//recursos remotos mediante el get generacion de promesas
 function RemoteResource($http,$q, baseUrl) {
   this.get = function() {
     var defered=$q.defer();
@@ -87,8 +42,25 @@ function RemoteResource($http,$q, baseUrl) {
     return promise;
     
   }
-}
 
+    this.listado = function() {
+    var defered=$q.defer();
+    var promise=defered.promise;
+    
+    $http({
+      method: 'GET',
+      url: baseUrl + '/listado.json'
+    }).success(function(data, status, headers, config) {
+      defered.resolve(data);
+    }).error(function(data, status, headers, config) {
+      defered.reject(status);
+    });
+    
+    return promise;
+    
+  }
+}
+//Provedor de recursos remotos , es el provedor que nos permite conectar las promesas con los datos json
 function RemoteResourceProvider() {
   var _baseUrl;
   this.setBaseUrl = function(baseUrl) {
@@ -109,7 +81,59 @@ app.config(['baseUrl', 'remoteResourceProvider',
   }
 ]);
 
+//configuracion de rutas para el redireccionamiento
+app.config(['$routeProvider',function($routeProvider) {
+ 
+  $routeProvider.when('/', {
+    templateUrl: "login.html",
+    controller: "MainController"
+  }); 
+ 
+  $routeProvider.when('/Orden/generadas', {
+    templateUrl: "Ordenes_generadas.html",
+    controller: "OrdenesgeneradasController",
+    resolve: {
+      generadas:['remoteResource',function(remoteResource) {
+        return remoteResource.listado();
+      }]
+    }
+  });
+     
+  $routeProvider.when('/Orden/generar', {
+    templateUrl: "Orden_de_trabajo.html",
+    controller: "ordenController"
+  });
 
+  $routeProvider.when('/Orden/detalle', {
+    templateUrl: "Detalle_discrepancias.html",
+    controller: "detalledisController"
+  });
+
+    $routeProvider.when('/Orden/discrepancia', {
+    templateUrl: "Discrepancias.html",
+    controller: ""
+  });      
+        $routeProvider.when('/Inventario/alta', {
+    templateUrl: "inventario.html",
+    controller: ""
+  });     
+  /*$routeProvider.when('/seguro/edit/:idSeguro', {
+    templateUrl: "Orden_de_trabajo.html",
+    controller: "DetalleSeguroController",
+    resolve: {
+      seguro:['remoteResource','$route',function(remoteResource,$route) {
+        return remoteResource.get($route.current.params.idSeguro);
+      }]
+    }
+  });*/
+   
+  $routeProvider.otherwise({
+        redirectTo: '/'
+  });   
+ 
+}]);
+
+//directiva de angular para mostrar el calendario usando el plugin de Jquery
 app.directive('caDatepicker', [function(dateFormat) {
   return {
     restrict: 'A',
@@ -139,7 +163,8 @@ app.controller('ordenController', ['$scope', 'remoteResource','OrdenesService',f
     a_modelo:"",
     n_serie:"",
     a_t_vuelo:"",
-    a_t_aterrizaje:""
+    a_t_aterrizaje:"",
+    fechaCreacion: new Date()
   }
 
   remoteResource.get().then(function(fo) {
@@ -185,6 +210,14 @@ app.controller('detalledisController', ['$scope', 'remoteResource',function($sco
     }, function(status) {
       alert("Ha fallado la petición. Estado HTTP:" + status);
     });
+
+}]);
+app.controller("OrdenesgeneradasController", ['$scope', 'generadas',function($scope,generadas) {
+$scope.generadas = generadas;
+ fechaCreacion: new Date()
+}]);
+
+app.controller("InventarioController", ['$scope',function($scope) {
 
 }]);
 app.controller("MainController", ['$scope',function($scope) {
