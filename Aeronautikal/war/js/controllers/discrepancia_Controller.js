40f04a3e6eff2ce086cb417269b6abbe34ff83f9
Tikal <Tikal@TikalEscritorio>
2017-12-15 18:50:55 -0600

    editar discrepancia tabla

--- Aeronautikal/war/js/controllers/discrepancia_Controller.js
+++ Aeronautikal/war/js/controllers/discrepancia_Controller.js
@@ -10,6 +10,18 @@ app.service('DiscrepanciaServicio', [ '$http', '$q', function($http, $q) {
     return d.promise;
   }
 } ]);
+//servicio update discrepancia
+app.service('UpdateDiscrepanciaServicio', [ '$http', '$q', function($http, $q) {
+  this.update_discrepancia = function(Objeto) {
+    var d = $q.defer();
+    $http.post("/discrepancia/update/",Objeto).then(function(response) {
+      console.log(response);
+      d.resolve(response.data);
+    }, function(response) {
+    });
+    return d.promise;
+  }
+} ]);
 
 app.controller("DiscrepanciamuestraController", ['$scope','inv_consultas','discrepancias','foliarrastrado','DiscrepanciaServicio','insertaRequiServicio',function($scope,inv_consultas,discrepancias,foliarrastrado,DiscrepanciaServicio,insertaRequiServicio) {
 $scope.discrepancias =discrepancias;
@@ -54,10 +66,11 @@ console.log($scope.provincias);
         })         
   }
 }]);
-app.controller("EditarDiscrepanciaController", ['$scope','discrepancia',function($scope,discrepancia) {
+app.controller("EditarDiscrepanciaController", ['$scope','discrepancia','UpdateDiscrepanciaServicio',function($scope,discrepancia,UpdateDiscrepanciaServicio) {
 $scope.discrepancia =discrepancia;
+$scope.eventos=[];
  //$scope para rellenar los campos y poder editarlos
- $scope.discrepancia = {
+ $scope.discrepancia_fo = {
     folio:undefined,
     fechaApertura:new Date(),
     taller:"",
@@ -65,14 +78,40 @@ $scope.discrepancia =discrepancia;
     descripcion:"",
     accion:"",
     folio_componente:undefined,
-    numero_piezas:undefined
+    numero_piezas:undefined,
+    entos:$scope.eventos
   }
  //$scope para retener la informacion en el front
  $scope.evento_tabla = {
-    fechaApertura:new Date(),
     nombreEvento:"",
     duracion:undefined,
-    cosoto:undefined
+    costo:undefined
   }
+
+$scope.Agregar=function(){
+  
+      
+      lista={
+          fechaApertura:new Date(),
+          nombreEvento:$scope.evento_tabla.nombreEvento,
+          duracion:$scope.evento_tabla.duracion,
+          costo:$scope.evento_tabla.costo
+          }
+          console.log(lista);
+      $scope.eventos.push(lista);
+
+      }    
+
+$scope.guardar_edit=function(){
+  
+      console.log($scope.discrepancia_fo);
+      UpdateDiscrepanciaServicio.update_discrepancia($scope.discrepancia_fo).then(
+        function(data) {
+          console.log(data);
+          alert("Los datos aqui se habrían enviado al servidor  y estarían validados en la parte cliente");
+  
+        })
+
+      }   
 
 }]);
