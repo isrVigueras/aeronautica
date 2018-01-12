8d73979a62223de539918d266644d19801d366bc
Tikal <Tikal@TikalEscritorio>
2018-01-12 17:54:03 -0600

    detalle vale

--- Aeronautikal/war/js/controllers/vales_Controller.js
+++ Aeronautikal/war/js/controllers/vales_Controller.js
@@ -1,8 +1,72 @@
-app.controller("MuestraValesController", ['$scope','discrepancia_vale',function($scope,discrepancia_vale) {
+//servicio Cerrar Vale
+app.service('CerrarValeService', [ '$http', '$q', function($http, $q) {
+  this.Cerrar_Vale = function(idVale) {
+    var d = $q.defer();
+    $http.post("/vale/cerrarVale/"+idVale).then(function(response) {
+      console.log(response);
+      d.resolve(response.data);
+    }, function(response) {
+    });
+    return d.promise;
+  }
+} ]);
+
+//filtro de vales
+app.filter("filtroV",["$filter",function($filter) {
+  var filterFn=$filter("filter");
+   
+  /** Transforma el texto quitando todos los acentos diéresis, etc. **/
+  function normalize(texto) {
+  	texto=texto+'';
+    texto = texto.replace(/[áàäâ]/g, "a");
+    texto = texto.replace(/[éèëê]/g, "e");
+    texto = texto.replace(/[íìïî]/g, "i");
+    texto = texto.replace(/[óòôö]/g, "o");
+    texto = texto.replace(/[úùüü]/g, "u");
+    texto = texto.toUpperCase();
+    return texto;
+  }
+    
+  /** Esta función es el comparator en el filter **/
+  function comparator(actual, expected) {
+      if (normalize(actual).indexOf(normalize(expected))>=0) {
+        return true;
+      } else {
+        return false;
+      }
+  }
+   
+  /** Este es realmente el filtro **/
+  function filtroV(array,expression) {
+    //Lo único que hace es llamar al filter original pero pasado
+    //la nueva función de comparator
+    return filterFn(array,expression,comparator)
+  }
+   
+  return filtroV;
+   
+}]);
+app.controller("MuestraValesController", ['$scope','discrepancia_vale','CerrarValeService',function($scope,discrepancia_vale,CerrarValeService) {
  $scope.discrepancia_vale =discrepancia_vale;
+ $scope.filtro = {
+      discrepancia: ""
+    }
   console.log($scope.discrepancia_vale);
-   $scope.muestra=function(data) {
+
+   $scope.muestraV=function(data) {
     console.log(data);
-    $scope.detalle_componentes = data; 
+    $scope.detalle_Vale = data; 
+  }
+
+   $scope.Despachar_Vale=function(id) {
+    console.log(id);
+    CerrarValeService.Cerrar_Vale(id).then(
+        function(data) {
+          console.log(data);
+           location.reload();
+          alert("Este vale sera cerrado definitivamente por el sistema");
+          //alert("Los datos aqui se habrían enviado al servidor  y estarían validados en la parte cliente");
+            location.href="#/Vales/consulta";
+        }) 
   }
 }]);
