1296ffdb7031cd62a5336a5aaa21b87ecd0d8ccf
Tikal <Tikal@TikalEscritorio>
2018-02-15 17:50:01 -0600

    comit para angy

--- Aeronautikal/war/js/controllers/orden_Controller.js
+++ Aeronautikal/war/js/controllers/orden_Controller.js
@@ -1,6 +1,6 @@
 //servicio para mandar los datos del formulario a la ruta del maping del controlador java mediante post
 app.service('OrdenesService', [ '$http', '$q', function($http, $q) {
-  this.genera_orden = function(fo) {
+  this.genera_orden = function(id,fo) {
     var d = $q.defer();
     /*var enviar = {
       lista : folio,
@@ -15,10 +15,15 @@ app.service('OrdenesService', [ '$http', '$q', function($http, $q) {
               a_t_vuelo,
               a_t_aterrizaje
     }*/
-    $http.post("/orden/add/",fo).then(function(response) {
+    $http.post("/orden/add/"+id,fo).then(function(response) {
       console.log(response);
       d.resolve(response.data);
     }, function(response) {
+         if(response.status==403){
+      alert("No tienes Permisos");
+      location.href="#/Inicio/paginaPrincipal";
+      //$rootScope.authenticated = false;
+                              }
     });
     return d.promise;
   }
@@ -36,8 +41,9 @@ app.service('OrdenenDocumentoService', [ '$http', '$q', function($http, $q) {
   }
 } ]);
 //Controlador de orden
-app.controller('ordenController', ['$scope','OrdenesService','new_folio','empresas','aeronaves',function($scope,OrdenesService,new_folio,empresas,aeronaves) {
+app.controller('ordenController', ['$scope','OrdenesService','new_folio','empresas','aeronaves','$cookies',function($scope,OrdenesService,new_folio,empresas,aeronaves,$cookies) {
  //$scope.fo = {}; alamcenar los datos en mi objeto para vidarlos
+  console.log($cookies.cosa);
  $scope.empresas = empresas;
  $scope.aeronaves=aeronaves;
  $scope.new_folio = new_folio;
@@ -59,7 +65,7 @@ $scope.new_folio = new_folio;
     if ($scope.form.$valid) {
       console.log($scope.fo)
       //alert("variable comprobada: "+$scope.fo.con_nombre+" y la fecha "+ $scope.fo.fechaApertura+"folio: "+ $scope.new_folio );
-      OrdenesService.genera_orden($scope.fo).then(
+      OrdenesService.genera_orden($cookies.cosa,$scope.fo).then(
         function(data) {
           console.log(data);
          // alert("Los datos aqui se habrían enviado al servidor  y estarían validados en la parte cliente");
