8cf0cb3add960808d0e444a8ad4bd65cd1b85fc4
Lenovo <Lenovo@LAPTOP-HNU12MCS>
2018-04-09 12:38:34 -0500

    reqs

--- Aeronautikal/src/com/tikal/aeronautikal/controller/AlertaController.java
+++ Aeronautikal/src/com/tikal/aeronautikal/controller/AlertaController.java
@@ -44,39 +44,36 @@ public class AlertaController {
 	   @RequestMapping(value = { "/findAlertas" }, method = RequestMethod.GET, produces = "application/json")
 			public void findComp(HttpServletResponse response, HttpServletRequest request) throws IOException {
 				AsignadorDeCharset.asignar(request, response);
-			//	List<List<ComponenteEntity>,List<RequisicionEntity>> ldel = new ArrayList<List<ComponenteEntity>,List<RequisicionEntity>>();
 				List<ComponenteEntity> lista = componenteDao.getMaxMin();
-				//System.out.println("Lista:");
+				System.out.println("Lista:");
 				
 				if (lista.size()==0) {
 					lista = new ArrayList<ComponenteEntity>();
-			
 					System.out.println("No hay componentes en alerta");
-					//response.getWriter().println(JsonConvertidor.toJson(lista));
-				}else{
-					System.out.println("Los componentes en alerta son :"+lista);
-					//response.getWriter().println(JsonConvertidor.toJson(lista));
 				}
 				
-				///para requisiciones
-				
-				
-				List<RequisicionEntity> reqs = requisicionDao.getAll();
-				System.out.println("Reqs:"+reqs);
-				
-				if (reqs.size()==0) {
-					reqs = new ArrayList<RequisicionEntity>();
-					System.out.println("No hay requisiciones");
-					//response.getWriter().println(JsonConvertidor.toJson(reqs));
-				}else{
-					System.out.println("Las requisiciones son :"+reqs);
-					//response.getWriter().println(JsonConvertidor.toJson(reqs));
-				}
-				
+				System.out.println("Los componentes en alerta son :"+lista);
 				response.getWriter().println(JsonConvertidor.toJson(lista));
-				response.getWriter().println(JsonConvertidor.toJson(reqs));
-				
 
 			}
+	   
+	   @RequestMapping(value = { "/findRequisiciones" }, method = RequestMethod.GET, produces = "application/json")
+		public void findReq(HttpServletResponse response, HttpServletRequest request) throws IOException {
+			AsignadorDeCharset.asignar(request, response);
+			
+			
+			
+			List<RequisicionEntity> reqs = requisicionDao.getAll();
+			System.out.println("Reqs:"+reqs);
+			
+			if (reqs.size()==0) {
+				reqs = new ArrayList<RequisicionEntity>();
+				System.out.println("No hay requisiciones");
+			}
+			
+			System.out.println("Las requisiciones son :"+reqs);
+			response.getWriter().println(JsonConvertidor.toJson(reqs));
+
+		}
 	   
 }
