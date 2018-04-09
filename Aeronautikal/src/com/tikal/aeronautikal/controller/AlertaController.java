36316a8d7236ad32199d5b6a62b4b6f64b208cfe
Lenovo <Lenovo@LAPTOP-HNU12MCS>
2018-04-09 10:37:14 -0500

    controller para alerta de requisiciones

--- Aeronautikal/src/com/tikal/aeronautikal/controller/AlertaController.java
+++ Aeronautikal/src/com/tikal/aeronautikal/controller/AlertaController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.tikal.aeronautikal.dao.ComponenteDao;
+import com.tikal.aeronautikal.dao.RequisicionDao;
 import com.tikal.aeronautikal.entity.EmpresaEntity;
+import com.tikal.aeronautikal.entity.RequisicionEntity;
 import com.tikal.aeronautikal.entity.otBody.ComponenteEntity;
 import com.tikal.aeronautikal.util.AsignadorDeCharset;
 import com.tikal.aeronautikal.util.JsonConvertidor;
@@ -29,6 +31,10 @@ public class AlertaController {
 	@Qualifier("componenteDao")
 	ComponenteDao componenteDao;
 	
+	@Autowired
+	@Qualifier("requisicionDao")
+	RequisicionDao requisicionDao;
+	
 	 @RequestMapping(value={"/prueba"},method = RequestMethod.GET)
 	   public void prueba(HttpServletResponse response, HttpServletRequest request) throws IOException {
 		   response.getWriter().println("Prueba del mètodo PROBAR en Orden de trabajo");
@@ -39,14 +45,30 @@ public class AlertaController {
 			public void findComp(HttpServletResponse response, HttpServletRequest request) throws IOException {
 				AsignadorDeCharset.asignar(request, response);
 				List<ComponenteEntity> lista = componenteDao.getMaxMin();
-				System.out.println("Lista:");
+				//System.out.println("Lista:");
 				
 				if (lista.size()==0) {
-					lista = new ArrayList<ComponenteEntity>();
+					//lista = new ArrayList<ComponenteEntity>();
+			
 					System.out.println("No hay componentes en alerta");
+					response.getWriter().println(0);
 				}
+				
+				///para requisiciones
+				
+				
+				List<RequisicionEntity> reqs = requisicionDao.getAll();
+				System.out.println("Reqs:"+reqs);
+				
+				if (reqs.size()==0) {
+					//reqs = new ArrayList<RequisicionEntity>();
+					System.out.println("No hay requisiciones");
+					response.getWriter().println(0);
+				}
+				
 				System.out.println("Los componentes en alerta son :"+lista);
 				response.getWriter().println(JsonConvertidor.toJson(lista));
+				response.getWriter().println(JsonConvertidor.toJson(reqs));
 
 			}
 	   
